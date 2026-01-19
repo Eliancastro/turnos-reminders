52066674d24bbf7f39fbc089633a3464f650e076
ElianCastro <eliancastro98@gmail.com>
2026-01-19 00:15:27 -0300

    fix server final

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post("/api/turnos", async (req, res) => {
     // MAIL EN SEGUNDO PLANO
     const msg = {
       to: email,
-      from: "elian <eliancastro98@gmail.com>", // funciona sin dominio propio
+      from: "Turnos <no-reply@resend.dev>", // funciona sin dominio propio
       subject: "Turno confirmado ✅",
       html: `
         <h2>Turno reservado</h2>
